165f5c931df0f3230cf678888206d506e9602290
noorullah-riu <noorullah.riu@gmail.com>
2026-04-29 13:44:39 +0500

    gg

--- Numpy/numPY.py
+++ Numpy/numPY.py
@@ -15,6 +15,7 @@
 
 import numpy as NP
 
+seprater="<-------------------------------->"
 arrayX= NP.array([1,2,3,4]);
 print(arrayX,'Printng Array');
 print(type(arrayX),'type of arrayX')
@@ -38,6 +39,7 @@ import timeit
 var1 = 900
 var2 = 100
 
+print(seprater)
 execution_time = timeit.timeit(
     'var1 * var2',
     globals=globals(),
@@ -46,6 +48,8 @@ execution_time = timeit.timeit(
 print("Execution time:", execution_time)
 
 
+
+print(seprater)
 # Array in numpy
 # 1 Dimenssion array in NP
 varList=[1,2,3,4]
@@ -57,45 +61,112 @@ print(varArr,'1 D Array from list ====>')
 # name = input("Enter your name: ")
 # print("Hello", name)
 
+
+print(seprater)
 #2D 3D and multidimennion Array 
 Array2D=NP.array([[1,2,3,4]])
 print(Array2D,Array2D.ndim,'Dimmession Array')
 
+
+print(seprater)
 Array3D=NP.array([[[1,2,3,4]]])
 print(Array3D,Array3D.ndim,'Dimmession Array')
 
+
+print(seprater)
 ArrayND=NP.array([1,2,3,4],ndmin=10)
 print(ArrayND,ArrayND.ndim,'Dimmession Array')
 
 
+print(seprater)
 # Zero matric
 arr_Zero=NP.zeros((3,3))
 print(arr_Zero,'Zero matric cration 3,3')
 
+
+print(seprater)
 # Ones matric
 arr_Ones=NP.ones((3,3))
 print(arr_Ones,'Ones matric cration 3,3')
+print(seprater)
 
+
+print(seprater)
 # Diagonal or identity matric
 arr_Diag=NP.eye(3,3)
 print(arr_Diag,'Identity matric cration')
 
+print(seprater)
 # Range matric-- create elements in a range
 arr_Range=NP.arange(8)
 print(arr_Range,'Range matric cration')
 
+print(seprater)
 # Line Space in array kinda crate a gap in elements
 arr_Space=NP.linspace(0,20,num=10)
 print(arr_Space,'Space matric cration')
+
+print(seprater)
 
 # Random Package and its functions for generating arrays with data
 randFun=NP.random.rand(2)
 print(randFun,'rand fun with 2 values single dimenssion array')
 
+print(seprater)
 
 randFun2=NP.random.rand(2,4)
 print(randFun2,'rand fun with 2 values single dimenssion array')
  
+ 
+ 
+ # randn which returns positive and negative numbers close to zero
+print(seprater)
+randnFun=NP.random.randn(2)
+print(randnFun,'randn which returns positive and negative numbers close to zero')
+
+ # ranf which returns numbers close to zero
+print(seprater)
+ranfFun=NP.random.ranf(4)
+print(ranfFun,'ranf which returns numbers close to zero')
+
+ # randint which returns numbers between range
+print(seprater)
+randintFun=NP.random.randint(1,5,10)
+print(randintFun,'randint which returns numbers between range, min,max,total value')
+
+
+
+# Data types in Numpy Arras Boleian , int , float 
+print(seprater)
+var3 =NP.array([1,2,3,4,5])
+print('datatype of var3 ',var3.dtype)
+
+print(seprater)
+var4 =NP.array([1.1,2.2,3.1,4,5])
+print('datatype of var4 ',var4.dtype)
+
+print(seprater)
+var5 =NP.array(['A','B','C'])
+print('datatype of var5 ',var5.dtype)
+
+print(seprater)
+var6 =NP.array([1.1,2.2,3.1,4,5],dtype=NP.int16)
+print('datatype of var6 from float to int converted ',var6.dtype)
+
+print(seprater)
+var7 =NP.array([1.1,2.2,3.1,4,5],dtype='S')
+print('datatype of var7 from float to string ',var7 .dtype)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
